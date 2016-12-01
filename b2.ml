823956045588767a58ca086bb1aa7bf7bc1d563f
zach shipko <zachshipko@gmail.com>
2016-11-30 21:46:55 -0800

    update

--- b2.ml
+++ b2.ml
@@ -7,67 +7,115 @@ open B2_client
 let auth_endpoint = "https://api.backblazeb2.com"
 let endpoint_base = ref "https://api.backblazeb2.com"
 
+
 module V1 = struct
 
-    let endpoint path = !endpoint_base ^ "/b2api/v1/b2_" ^ path
+    module Token = struct
+        type t = {
+            accountId: string;
+            authorizationToken: string;
+            apiUrl: string;
+            downloadUrl: string;
+            minimumPartSize: int;
+        }
+    end
 
-    type token = {
-        accountId: string;
-        authorizationToken: string;
-        apiUrl: string;
-        downloadUrl: string;
-        minimumPartSize: int;
-    }
+    module Download_authorization = struct
+        type t = {
+            bucketId: string;
+            fileNamePrefix: string;
+            authorizationToken: string;
+        }
+    end
+
+    module File = struct
+        type t = {
+            fileId : string;
+            accountId : string;
+            bucketId : string;
+            fileName : string;
+        }
+    end
+
+    module File_version = struct
+        type t = {
+            fileId: string;
+            fileName: string;
+        }
+    end
+
+    module File_info = struct
+        type t = {
+            fileId: string;
+            fileName: string;
+            accountId: string;
+            contentSha1: Cstruct.t;
+            bucketId: string;
+            contentLength: int64;
+            contentType: string;
+            fileInfo: Ezjsonm.value;
+            action: string;
+            uploadTimestamp: Int64.t;
+        }
+    end
+
+    module Partial_file_info = struct
+        type t = {
+            fileId: string;
+            fileName: string;
+            accountId: string;
+            bucketId: string;
+            contentType: string;
+            fileInfo: Ezjsonm.value;
+            uploadTimestamp: Int64.t;
+        }
+    end
+
+    module Upload_url = struct
+        type t = {
+            uploadUrl: string;
+            authorizationToken: string;
+        }
+    end
+
+    module Part = struct
+        type t = {
+            fileId: string;
+            partNumber: int;
+            contentLength: int64;
+            contentSha1: Cstruct.t;
+        }
+    end
+
+    module Bucket = struct
+        (* NOTE: lifecycleRules is not yet implemented *)
+        type t = {
+            accountId: string;
+            bucketId: string;
+            bucketName: string;
+            bucketType: string;
+            bucketInfo: Ezjsonm.value;
+            revision: Int64.t;
+        }
+    end
+
+
+    let endpoint path = !endpoint_base ^ "/b2api/v1/b2_" ^ path
 
     (* Makes a new header object with the given token *)
     let make_header ?fields:(fields=[]) token =
         Cohttp.Header.of_list ([
-            "Authorization", token.authorizationToken
+            "Authorization", token.Token.authorizationToken
         ] @ fields)
 
-    type file = {
-        fileId : string;
-        accountId : string;
-        bucketId : string;
-        fileName : string;
-    }
-
-    type file_version = {
-        fileId: string;
-        fileName: string;
-    }
-
-    type file_info = {
-        fileId: string;
-        fileName: string;
-        accountId: string;
-        contentSha1: Cstruct.t;
-        bucketId: string;
-        contentLength: int64;
-        contentType: string;
-        fileInfo: Ezjsonm.value;
-        action: string;
-        uploadTimestamp: Int64.t;
-    }
-
-    type partial_file_info = {
-        fileId: string;
-        fileName: string;
-        accountId: string;
-        bucketId: string;
-        contentType: string;
-        fileInfo: Ezjsonm.value;
-        uploadTimestamp: Int64.t;
-    }
-
-    let authorize_account (accountId: string) (applicationKey : string) : token Lwt.t =
+    let authorize_account (accountId: string) (applicationKey : string) =
         let encoded = Nocrypto.Base64.encode (Cstruct.of_string (accountId ^ ":" ^ applicationKey)) |> Cstruct.to_string in
         let headers = Cohttp.Header.of_list [
             "Authorization", "Basic " ^ encoded
         ] in
         B2_client.get_json headers (auth_endpoint ^ "/b2api/v1/b2_authorize_account")
         >|= handle_error (fun j ->
-            let _ = endpoint_base := find_string j ["apiUrl"] in {
+            let _ = endpoint_base := find_string j ["apiUrl"] in Token.{
             accountId = account_id j;
             authorizationToken = find_string j ["authorizationToken"];
             apiUrl= find_string j ["apiUrl"];
@@ -77,31 +125,25 @@ module V1 = struct
 
     (* Files *)
 
-    let delete_file_version token (fileName : string) (fileId : string) : file_version Lwt.t =
+    let delete_file_version token (fileName : string) (fileId : string) =
         let headers = make_header token in
         B2_client.post_json ~json:(`O [
             "fileName", `String fileName;
             "fileId", `String fileId;
         ]) headers (endpoint "delete_file_version")
-        >|= handle_error (fun j -> {
+        >|= handle_error (fun j -> File_version.{
             fileId = file_id j;
             fileName = file_name j;
         })
 
-   type download_authorization = {
-        bucketId: string;
-        fileNamePrefix: string;
-        authorizationToken: string;
-    }
-
-    let get_download_authorization token bucketId fileNamePrefix validDurationInSeconds : download_authorization Lwt.t =
+    let get_download_authorization token bucketId fileNamePrefix validDurationInSeconds =
         let headers = make_header token in
         B2_client.post_json ~json:(`O [
             "bucketId", `String bucketId;
             "fileNamePrefix", `String fileNamePrefix;
             "validDurationInSeconds", `String (string_of_int validDurationInSeconds);
         ]) headers (endpoint "get_download_authorization")
-        >|= handle_error (fun j -> {
+        >|= handle_error (fun j -> Download_authorization.{
             bucketId = bucket_id j;
             fileNamePrefix = find_string j ["fileNamePrefix"];
             authorizationToken = find_string j ["authorizationToken"];
@@ -110,22 +152,22 @@ module V1 = struct
     let download_file_by_id ?token ?url:(url="") (fileId: string) : string Lwt.t =
         let headers, url = match token with
         | Some tok ->
-            make_header tok, tok.downloadUrl
+            make_header tok, tok.Token.downloadUrl
         | None -> Cohttp.Header.init (), url in
-        B2_client.get headers (url ^ "/download_file_by_id?fileId=" ^ fileId)
+        B2_client.get headers (endpoint "download_file_by_id?fileId=" ^ fileId)
 
     let download_file_by_name ?token ?auth ?url:(url="") (fileName: string) : string Lwt.t =
         let headers, url = match token with
         | Some tok ->
-            make_header tok, tok.downloadUrl
+            make_header tok, tok.Token.downloadUrl
         | None -> (match auth with
         | Some a -> Cohttp.Header.of_list [
-            "Authorization", a.authorizationToken
+            "Authorization", a.Download_authorization.authorizationToken
         ]
         | None -> Cohttp.Header.init ()), url in
-        B2_client.get headers (url ^ "/download_file_by_name?fileName=" ^ fileName)
+        B2_client.get headers (Filename.concat url fileName)
 
-    let find_all_file_info j : file_info = {
+    let find_all_file_info j = File_info.{
         fileId = file_id j;
         fileName = file_name j;
         accountId = account_id j;
@@ -138,7 +180,7 @@ module V1 = struct
         uploadTimestamp = find_int j ["uploadTimestamp"];
     }
 
-    let find_all_partial_file_info j : partial_file_info = {
+    let find_all_partial_file_info j = Partial_file_info.{
         fileId = file_id j;
         fileName = file_name j;
         accountId = account_id j;
@@ -148,13 +190,13 @@ module V1 = struct
         uploadTimestamp = find_int j ["uploadTimestamp"];
     }
 
-    let get_file_info token fileId : file_info Lwt.t =
+    let get_file_info token fileId =
         B2_client.post_json ~json:(`O [
             "fileId", `String fileId;
         ]) (make_header token) (endpoint "get_file_info")
         >|= handle_error find_all_file_info
 
-    let hide_file token bucketId fileName : file_info Lwt.t =
+    let hide_file token bucketId fileName =
         let headers = make_header token in
         B2_client.post_json ~json:(`O [
             "bucketId", `String bucketId;
@@ -165,7 +207,7 @@ module V1 = struct
     let list_file_names token ?startFileName:(startFileName="")
                               ?maxFileCount:(maxFileCount=0)
                               ?prefix:(prefix="")
-                              ?delimiter bucketId : (file_info list * string) Lwt.t =
+                              ?delimiter bucketId =
         let headers = make_header token in
         B2_client.post_json ~json:(`O ([
             "bucketId", `String bucketId;
@@ -186,7 +228,7 @@ module V1 = struct
                                  ?startFileId:(startFileId="")
                                  ?maxFileCount:(maxFileCount=0)
                                  ?prefix:(prefix="")
-                                 ?delimiter bucketId : (file_info list * string * string) Lwt.t =
+                                 ?delimiter bucketId =
         let headers = make_header token in
         B2_client.post_json ~json:(`O ([
             "bucketId", `String bucketId;
@@ -204,11 +246,6 @@ module V1 = struct
             |> Ezjsonm.get_list find_all_file_info
             |> fun l -> l, find_string j ["nextFileName"], find_string j ["nextFileId"])
 
-    type upload_url = {
-        uploadUrl: string;
-        authorizationToken: string;
-    }
-
     let get_hex = function
         | `Hex s -> s
         | _ -> ""
@@ -218,11 +255,11 @@ module V1 = struct
         let _ = Nocrypto.Hash.SHA1.feed h (Cstruct.of_string s) in
         Nocrypto.Hash.SHA1.get h |> Hex.of_cstruct |> get_hex
 
-    let upload_file (url : upload_url) ?contentType:(contentType="b2/x-auto") ?fileInfo:(fileInfo=[]) (data : char Lwt_stream.t) fileName  : file_info Lwt.t =
+    let upload_file (url : Upload_url.t) ?contentType:(contentType="b2/x-auto") ?fileInfo:(fileInfo=[]) (data : char Lwt_stream.t) fileName  =
 
         Lwt_stream.to_string data
         >>= fun data ->
-            let headers = ref (Cohttp.Header.of_list [
+            let headers = ref (Cohttp.Header.of_list Upload_url.[
                 "Authorization", url.authorizationToken;
                 "X-Bz-File-Name", fileName;
                 "Content-Type", contentType;
@@ -231,23 +268,23 @@ module V1 = struct
             ]) in
             let _ = List.iteri (fun i (k, v) ->
                 headers := Cohttp.Header.replace !headers ("X-Bz-Info-" ^ k) (Ezjsonm.to_string v)) fileInfo in
-            B2_client.post ~body:(`String data) !headers url.uploadUrl
+            B2_client.post ~body:(`String data) !headers url.Upload_url.uploadUrl
         >|= Ezjsonm.from_string
         >|= handle_error find_all_file_info
 
     (* Large files *)
 
-    let cancel_large_file token (fileId : string) : file Lwt.t =
+    let cancel_large_file token (fileId : string)  =
         let headers = make_header token in
         B2_client.post_json ~json:(`O ["fileId", `String fileId]) headers (endpoint "cancel_large_file")
-        >|= handle_error (fun j ->{
+        >|= handle_error (fun j -> File.{
             fileId = file_id j;
             accountId = account_id j;
             bucketId = bucket_id j;
             fileName = file_name j;
         })
 
-    let start_large_file token ?contentType:(contentType="b2/x-auto") ?fileInfo:(fileInfo=[]) bucketId fileName : partial_file_info Lwt.t =
+    let start_large_file token ?contentType:(contentType="b2/x-auto") ?fileInfo:(fileInfo=[]) bucketId fileName =
         let headers = make_header token in
         B2_client.post_json  ~json:(`O [
             "bucketId", `String bucketId;
@@ -256,7 +293,7 @@ module V1 = struct
         ]) headers (endpoint "start_large_file")
         >|= handle_error find_all_partial_file_info
 
-    let finish_large_file token fileId partSha1Array : file_info Lwt.t =
+    let finish_large_file token fileId partSha1Array =
         let headers = make_header token in
         B2_client.post_json ~json:(`O [
             "fileId", `String fileId;
@@ -265,32 +302,25 @@ module V1 = struct
         ]) headers (endpoint "finish_large_file")
         >|= handle_error find_all_file_info
 
-    type part = {
-        fileId: string;
-        partNumber: int;
-        contentLength: int64;
-        contentSha1: Cstruct.t;
-    }
-
-    let upload_part (url : upload_url) ?contentType:(contentType="b2/x-auto") (data : char Lwt_stream.t) partNum  : part Lwt.t =
+    let upload_part (url : Upload_url.t) ?contentType:(contentType="b2/x-auto") (data : char Lwt_stream.t) partNum =
         Lwt_stream.to_string data
         >>= fun data ->
             let headers = ref (Cohttp.Header.of_list [
-                "Authorization", url.authorizationToken;
+                "Authorization", url.Upload_url.authorizationToken;
                 "X-Bz-Part-Number", string_of_int partNum;
                 "Content-Length", string_of_int (String.length data);
                 "X-Bz-Content-Sha1", hash_string data;
             ]) in
-            B2_client.post ~body:(`String data) !headers url.uploadUrl
+            B2_client.post ~body:(`String data) !headers url.Upload_url.uploadUrl
         >|= Ezjsonm.from_string
-        >|= handle_error (fun j -> {
+        >|= handle_error (fun j -> Part.{
             fileId = file_id j;
             partNumber = find_int j ["partNumber"] |> Int64.to_int;
             contentLength = find_int j ["contentLength"];
             contentSha1 = find_cstruct j ["contentSha1"];
         })
 
-    let list_parts token fileId : part list Lwt.t =
+    let list_parts token fileId =
         let headers = make_header token in
         B2_client.post_json ~json:(`O [
             "fileId", `String fileId;
@@ -298,7 +328,7 @@ module V1 = struct
         ]) headers (endpoint "list_parts")
         >|= handle_error (fun j ->
             Ezjsonm.find j ["parts"]
-            |> Ezjsonm.get_list (fun j -> {
+            |> Ezjsonm.get_list (fun j -> Part.{
                 fileId = file_id j;
                 partNumber = find_int j ["partNumber"] |> Int64.to_int;
                 contentLength = find_int j ["contentLength"];
@@ -308,17 +338,7 @@ module V1 = struct
 
     (* Buckets *)
 
-    (* NOTE: lifecycleRules is not yet implemented *)
-    type bucket = {
-        accountId: string;
-        bucketId: string;
-        bucketName: string;
-        bucketType: string;
-        bucketInfo: Ezjsonm.value;
-        revision: Int64.t;
-    }
-
-    let find_all_bucket j = {
+    let find_all_bucket j = Bucket.{
         accountId = account_id j;
         bucketId = bucket_id j;
         bucketName = bucket_name j;
@@ -327,7 +347,7 @@ module V1 = struct
         revision = revision j;
     }
 
-    let create_bucket token ?bucketInfo (bucketName: string) (bucketType: string) : bucket Lwt.t =
+    let create_bucket token ?bucketInfo (bucketName: string) (bucketType: string) =
         let headers = make_header token in
         let info = match bucketInfo with
             | Some jv -> ["bucketInfo", `O jv]
@@ -335,51 +355,51 @@ module V1 = struct
         B2_client.post_json ~json:(`O ([
             "bucketName", `String bucketName;
             "bucketType", `String bucketType;
-            "accountId", `String token.accountId;
+            "accountId", `String token.Token.accountId;
         ] @ info)) headers (endpoint "create_bucket")
         >|= handle_error find_all_bucket
 
-    let delete_bucket token (bucketId: string) : bucket Lwt.t =
+    let delete_bucket token (bucketId: string) =
         let headers = make_header token in
         B2_client.post_json ~json:(`O [
             "bucketId", `String bucketId;
-            "accountId", `String token.accountId;
+            "accountId", `String token.Token.accountId;
         ]) headers (endpoint "delete_bucket")
         >|= handle_error find_all_bucket
 
-    let get_upload_url token bucketId : upload_url Lwt.t =
+    let get_upload_url token bucketId =
         let headers = make_header token in
         B2_client.post_json ~json:(`O [
             "bucketId", `String bucketId;
         ]) headers (endpoint "get_upload_url")
-        >|= handle_error (fun j -> {
+        >|= handle_error (fun j -> Upload_url.{
             uploadUrl = find_string j ["uploadUrl"];
             authorizationToken = find_string j ["authorizationToken"];
         })
 
-    let get_upload_part_url token fileId : upload_url Lwt.t =
+    let get_upload_part_url token fileId =
         let headers = make_header token in
         B2_client.post_json ~json:(`O [
             "fileId", `String fileId;
         ]) headers (endpoint "get_upload_part_url")
-        >|= handle_error (fun j -> {
+        >|= handle_error (fun j -> Upload_url.{
             uploadUrl = find_string j ["uploadUrl"];
             authorizationToken = find_string j ["authorizationToken"];
         })
 
-    let list_buckets token : bucket list Lwt.t =
+    let list_buckets token =
         let headers = make_header token in
         B2_client.post_json ~json:(`O [
-            "accountId", `String token.accountId
+            "accountId", `String token.Token.accountId
         ]) headers (endpoint "list_buckets")
         >|= handle_error (fun j ->
             Ezjsonm.find j ["buckets"]
             |> Ezjsonm.get_list find_all_bucket)
 
-    let update_bucket token ?bucketType ?bucketInfo ?ifRevisionIs bucketId bucketName : bucket Lwt.t =
+    let update_bucket token ?bucketType ?bucketInfo ?ifRevisionIs bucketId bucketName =
         let headers = make_header token in
         let params = ref [
-            "accountId", `String token.accountId;
+            "accountId", `String token.Token.accountId;
             "bucketId", `String bucketId;
             "bucketName", `String bucketName;
         ] in
--- b2.mli
+++ b2.mli
@@ -2,95 +2,115 @@ val endpoint_base : string ref
 
 module V1 : sig
 
-    type token = {
-        accountId: string;
-        authorizationToken: string;
-        apiUrl: string;
-        downloadUrl: string;
-        minimumPartSize: int;
-    }
+    module Token : sig
+        type t = {
+            accountId: string;
+            authorizationToken: string;
+            apiUrl: string;
+            downloadUrl: string;
+            minimumPartSize: int;
+        }
+    end
 
-    type file = {
-        fileId : string;
-        accountId : string;
-        bucketId : string;
-        fileName : string;
-    }
+    module Download_authorization : sig
+        type t = {
+            bucketId: string;
+            fileNamePrefix: string;
+            authorizationToken: string;
+        }
+    end
 
-    type file_version = {
-        fileId: string;
-        fileName: string;
-    }
+    module File : sig
+        type t = {
+            fileId : string;
+            accountId : string;
+            bucketId : string;
+            fileName : string;
+        }
+    end
 
-    type file_info = {
-        fileId: string;
-        fileName: string;
-        accountId: string;
-        contentSha1: Cstruct.t;
-        bucketId: string;
-        contentLength: int64;
-        contentType: string;
-        fileInfo: Ezjsonm.value;
-        action: string;
-        uploadTimestamp: Int64.t;
-    }
+    module File_version : sig
+        type t = {
+            fileId: string;
+            fileName: string;
+        }
+    end
 
-    type partial_file_info = {
-        fileId: string;
-        fileName: string;
-        accountId: string;
-        bucketId: string;
-        contentType: string;
-        fileInfo: Ezjsonm.value;
-        uploadTimestamp: Int64.t;
-    }
+    module File_info : sig
+        type t = {
+            fileId: string;
+            fileName: string;
+            accountId: string;
+            contentSha1: Cstruct.t;
+            bucketId: string;
+            contentLength: int64;
+            contentType: string;
+            fileInfo: Ezjsonm.value;
+            action: string;
+            uploadTimestamp: Int64.t;
+        }
+    end
 
-    type bucket = {
-        accountId: string;
-        bucketId: string;
-        bucketName: string;
-        bucketType: string;
-        bucketInfo: Ezjsonm.value;
-        revision: Int64.t;
-    }
+    module Partial_file_info : sig
+        type t = {
+            fileId: string;
+            fileName: string;
+            accountId: string;
+            bucketId: string;
+            contentType: string;
+            fileInfo: Ezjsonm.value;
+            uploadTimestamp: Int64.t;
+        }
+    end
 
-    type upload_url = {
-        uploadUrl: string;
-        authorizationToken: string;
-    }
+    module Upload_url : sig
+        type t = {
+            uploadUrl: string;
+            authorizationToken: string;
+        }
+    end
 
-    type download_authorization = {
-        bucketId: string;
-        fileNamePrefix: string;
-        authorizationToken: string;
-    }
+    module Part : sig
+        type t = {
+            fileId: string;
+            partNumber: int;
+            contentLength: int64;
+            contentSha1: Cstruct.t;
+        }
+    end
 
-    type part = {
-        fileId: string;
-        partNumber: int;
-        contentLength: int64;
-        contentSha1: Cstruct.t;
-    }
+    module Bucket : sig
+        (* NOTE: lifecycleRules is not yet implemented *)
+        type t = {
+            accountId: string;
+            bucketId: string;
+            bucketName: string;
+            bucketType: string;
+            bucketInfo: Ezjsonm.value;
+            revision: Int64.t;
+        }
+    end
 
-    val authorize_account : string -> string -> token Lwt.t
-    val cancel_large_file : token -> string -> file Lwt.t
-    val delete_file_version : token -> string -> string -> file_version Lwt.t
-    val get_download_authorization : token -> string -> string -> int -> download_authorization Lwt.t
-    val download_file_by_id : ?token:token -> ?url:string -> string -> string Lwt.t
-    val download_file_by_name : ?token:token -> ?auth:download_authorization -> ?url:string -> string -> string Lwt.t
-    val get_file_info : token -> string -> file_info Lwt.t
-    val hide_file : token -> string -> string -> file_info Lwt.t
-    val list_file_names : token -> ?startFileName:string -> ?maxFileCount:int -> ?prefix:string -> ?delimiter:string -> string -> (file_info list * string) Lwt.t
-    val list_file_versions : token -> ?startFileName:string -> ?startFileId:string -> ?maxFileCount:int -> ?prefix:string -> ?delimiter:string -> string -> (file_info list * string * string) Lwt.t
-    val upload_file : upload_url -> ?contentType:string -> ?fileInfo:(string * Ezjsonm.t) list -> char Lwt_stream.t -> string -> file_info Lwt.t
-    val start_large_file : token -> ?contentType:string -> ?fileInfo:(string * Ezjsonm.value) list -> string -> string -> partial_file_info Lwt.t
-    val finish_large_file : token -> string -> Cstruct.t list -> file_info Lwt.t
-    val upload_part : upload_url -> ?contentType:string -> char Lwt_stream.t -> int -> part Lwt.t
-    val list_parts : token -> string -> part list Lwt.t
-    val create_bucket : token -> ?bucketInfo:(string * Ezjsonm.value) list -> string -> string -> bucket Lwt.t
-    val delete_bucket : token -> string -> bucket Lwt.t
-    val get_upload_url : token -> string -> upload_url Lwt.t
-    val get_upload_part_url : token -> string -> upload_url Lwt.t
-    val list_buckets : token -> bucket list Lwt.t
-    val update_bucket : token -> ?bucketType:string -> ?bucketInfo:(string * Ezjsonm.value) list -> ?ifRevisionIs:Int64.t -> string -> string -> bucket Lwt.t
+
+    val authorize_account : string -> string -> Token.t Lwt.t
+    val cancel_large_file : Token.t -> string -> File.t Lwt.t
+    val delete_file_version : Token.t -> string -> string -> File_version.t Lwt.t
+    val get_download_authorization : Token.t -> string -> string -> int -> Download_authorization.t Lwt.t
+    val download_file_by_id : ?token:Token.t-> ?url:string -> string -> string Lwt.t
+    val download_file_by_name : ?token:Token.t -> ?auth:Download_authorization.t -> ?url:string -> string -> string Lwt.t
+    val get_file_info : Token.t -> string -> File_info.t Lwt.t
+    val hide_file : Token.t -> string -> string -> File_info.t Lwt.t
+    val list_file_names : Token.t -> ?startFileName:string -> ?maxFileCount:int -> ?prefix:string -> ?delimiter:string -> string -> (File_info.t list * string) Lwt.t
+    val list_file_versions : Token.t -> ?startFileName:string -> ?startFileId:string -> ?maxFileCount:int -> ?prefix:string -> ?delimiter:string -> string -> (File_info.t list * string * string) Lwt.t
+    val upload_file : Upload_url.t -> ?contentType:string -> ?fileInfo:(string * Ezjsonm.t) list -> char Lwt_stream.t -> string -> File_info.t Lwt.t
+    val start_large_file : Token.t -> ?contentType:string -> ?fileInfo:(string * Ezjsonm.value) list -> string -> string -> Partial_file_info.t Lwt.t
+    val finish_large_file : Token.t -> string -> Cstruct.t list -> File_info.t Lwt.t
+    val upload_part : Upload_url.t -> ?contentType:string -> char Lwt_stream.t -> int -> Part.t Lwt.t
+    val list_parts : Token.t -> string -> Part.t list Lwt.t
+    val create_bucket : Token.t -> ?bucketInfo:(string * Ezjsonm.value) list -> string -> string -> Bucket.t Lwt.t
+    val delete_bucket : Token.t -> string -> Bucket.t Lwt.t
+    val get_upload_url : Token.t -> string -> Upload_url.t Lwt.t
+    val get_upload_part_url : Token.t -> string -> Upload_url.t Lwt.t
+    val list_buckets : Token.t -> Bucket.t list Lwt.t
+    val update_bucket : Token.t -> ?bucketType:string -> ?bucketInfo:(string * Ezjsonm.value) list -> ?ifRevisionIs:Int64.t -> string -> string -> Bucket.t Lwt.t
 end
